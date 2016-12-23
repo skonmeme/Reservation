918940e3bcc81b9a0341d99b5e21d5d385fde301
Sung Gon Yi <skon@172.20.nate.com>
2016-12-23 10:33:50 +0900

    Initial Commit

---
+++ Reservation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reservation
+//
+//  Created by Sung Gon Yi on 23/12/2016.
+//  Copyright © 2016 skon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
