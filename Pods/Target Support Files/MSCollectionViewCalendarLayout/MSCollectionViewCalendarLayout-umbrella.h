0b1e6c867e2d4dd7031d3d0eb8efbacae2028088
Sung Gon Yi <skonmeme@gmail.com>
2017-01-02 11:11:28 +0900

    CocoaPod: MSCollectionViewCalendarLayout has been imported.

---
+++ Pods/Target Support Files/MSCollectionViewCalendarLayout/MSCollectionViewCalendarLayout-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "MSCollectionViewCalendarLayout.h"
+
+FOUNDATION_EXPORT double MSCollectionViewCalendarLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char MSCollectionViewCalendarLayoutVersionString[];
+
